b2587873266d2079dc0b3e5fe6973985b934d467
rlacobelle20 <rlacobelle02@gmail.com>
2021-07-09 16:39:09 -0400

    updated default class

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,14 @@ import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
 // the main class for the app
 export default class HappyBirthday {
-    
+    private text: MRE.Actor = null;
+    private cube: MRE.Actor = null;
+    private assets: MRE.AssetContainer;
+
+    constructor(private context: MRE.Context) {
+        this.context.onStarted(() => this.started());
+    }
+
+
 }
 
