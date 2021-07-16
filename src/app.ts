6b7a43fb263a2ce24d65420dd6f5767f121349b2
rlacobelle20 <rlacobelle02@gmail.com>
2021-07-16 10:39:28 -0400

    update -> added spinCupcake

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // imports repository for mre
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
+
 // the main class for the app
 export default class HappyBirthday {
     private text: MRE.Actor = null;
@@ -32,11 +33,8 @@ export default class HappyBirthday {
 			}
 		});
 
-        // ----------------------------------------------------------------------------------------------------
-
-        // Here we create an animation for our text actor. First we create animation data, which can be used on any
-		// actor. We'll reference that actor with the placeholder "text".
-		const spinAnimData = this.assets.createAnimationData(
+		// get cupcake to spin/rotate
+		const spinCupcake = this.assets.createAnimationData(
 			// The name is a unique identifier for this data. You can use it to find the data in the asset container,
 			// but it's merely descriptive in this sample.
 			"Spin",
@@ -52,77 +50,19 @@ export default class HappyBirthday {
 					easing: MRE.AnimationEaseCurves.Linear
 				}]
 			});
-		// Once the animation data is created, we can create a real animation from it.
-		spinAnimData.bind(
-			// We assign our text actor to the actor placeholder "text"
-			{ text: this.text },
-			// And set it to play immediately, and bounce back and forth from start to end
-			{ isPlaying: true, wrapMode: MRE.AnimationWrapMode.PingPong });
+			
+		// Once the animation data i
 
-		// Load a glTF model before we use it
-		const cubeData = await this.assets.loadGltf('altspace-cube.glb', "box");
+	}
 
-		// spawn a copy of the glTF model
-		this.cube = MRE.Actor.CreateFromPrefab(this.context, {
-			// using the data we loaded earlier
-			firstPrefabFrom: cubeData,
-			// Also apply the following generic actor properties.
-			actor: {
-				name: 'Altspace Cube',
-				// Parent the glTF model to the text actor, so the transform is relative to the text
-				parentId: this.text.id,
-				transform: {
-					local: {
-						position: { x: 0, y: -1, z: 0 },
-						scale: { x: 0.4, y: 0.4, z: 0.4 }
-					}
-				}
-			}
-		});
-
-		// Create some animations on the cube.
-		const flipAnimData = this.assets.createAnimationData(
-			// the animation name
-			"DoAFlip",
-			{ tracks: [{
-				// applies to the rotation of an unknown actor we'll refer to as "target"
-				target: MRE.ActorPath("target").transform.local.rotation,
-				// do a spin around the X axis over the course of one second
-				keyframes: this.generateSpinKeyframes(1.0, MRE.Vector3.Right()),
-				// and do it smoothly
-				easing: MRE.AnimationEaseCurves.Linear
-			}]}
-		);
-		// apply the animation to our cube
-		const flipAnim = await flipAnimData.bind({ target: this.cube });
-
-		// Set up cursor interaction. We add the input behavior ButtonBehavior to the cube.
-		// Button behaviors have two pairs of events: hover start/stop, and click start/stop.
-		const buttonBehavior = this.cube.setBehavior(MRE.ButtonBehavior);
-
-		// Trigger the grow/shrink animations on hover.
-		buttonBehavior.onHover('enter', () => {
-			// use the convenience function "AnimateTo" instead of creating the animation data in advance
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.5, y: 0.5, z: 0.5 } } } },
-				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
-			});
-		});
-		buttonBehavior.onHover('exit', () => {
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.4, y: 0.4, z: 0.4 } } } },
-				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
-			});
-		});
-
-		// When clicked, do a 360 sideways.
-		buttonBehavior.onClick(_ => {
-			flipAnim.play();
-		});
-    }
+	// planning for what is needed
 
 
+
+
+
+	// get cupcake to say happy birthday
+	// get fireworks to shoot out of cupcake when touched
+	// potentially get cubcake to change colors -> can pick from a directory/map/shelf
 }
 
